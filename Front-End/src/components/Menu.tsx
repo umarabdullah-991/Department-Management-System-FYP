cf56c1b07e2d296c18426981d491f4d1d34538db
Umar <umarabdullah.work@gmail.com>
2024-10-24 15:39:12 +0500

    Menu title list added

--- Front-End/src/components/Menu.tsx
+++ Front-End/src/components/Menu.tsx
@@ -1,3 +1,103 @@
+ 
+const menuItems = [
+    {
+      title: "MENU",
+      items: [
+        {
+          icon: "/home.png",
+          label: "Home",
+          href: "/",
+        },
+        {
+          icon: "/teacher.png",
+          label: "Teachers",
+          href: "/list/teachers",
+        },
+        {
+          icon: "/student.png",
+          label: "Students",
+          href: "/list/students",
+        },
+        {
+          icon: "/parent.png",
+          label: "Parents",
+          href: "/list/parents",
+        },
+        {
+          icon: "/subject.png",
+          label: "Subjects",
+          href: "/list/subjects",
+        },
+        {
+          icon: "/class.png",
+          label: "Classes",
+          href: "/list/classes",
+        },
+        {
+          icon: "/lesson.png",
+          label: "Lessons",
+          href: "/list/lessons",
+        },
+        {
+          icon: "/exam.png",
+          label: "Exams",
+          href: "/list/exams",
+        },
+        {
+          icon: "/assignment.png",
+          label: "Assignments",
+          href: "/list/assignments",
+        },
+        {
+          icon: "/result.png",
+          label: "Results",
+          href: "/list/results",
+        },
+        {
+          icon: "/attendance.png",
+          label: "Attendance",
+          href: "/list/attendance",
+        },
+        {
+          icon: "/calendar.png",
+          label: "Events",
+          href: "/list/events",
+        },
+        {
+          icon: "/message.png",
+          label: "Messages",
+          href: "/list/messages",
+        },
+        {
+          icon: "/announcement.png",
+          label: "Announcements",
+          href: "/list/announcements",
+          
+        },
+      ],
+    },
+    {
+      title: "OTHER",
+      items: [
+        {
+          icon: "/profile.png",
+          label: "Profile",
+          href: "/profile",
+        },
+        {
+          icon: "/setting.png",
+          label: "Settings",
+          href: "/settings",
+        },
+        {
+          icon: "/logout.png",
+          label: "Logout",
+          href: "/logout",
+        },
+      ],
+    },
+  ];
+  
 const Menu = () => {
     return (
         <div className = ''> Menu </div>
