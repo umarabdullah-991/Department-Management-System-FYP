3bade6f08f5704147168bf749c4969e29d7edec4
Umar <umarabdullah.work@gmail.com>
2024-10-24 15:10:16 +0500

    Layout file in Dashboard updated

--- Front-End/src/app/layout.tsx
+++ Front-End/src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Department Management System",
-  description: "Department Management System",
+  title: "lama-dev-next-dashboard",
+  description: "lama-dev-next-dashboard",
 };
 
 export default function RootLayout({
