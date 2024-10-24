4e70cbafdccaeae528285f9cac008c32e6686fc3
Syed <amanxsyed@gmail.com>
2024-10-24 12:42:55 +0500

    Change names

--- Front-End/src/app/layout.tsx
+++ Front-End/src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
-  description: "Next.js School Management System",
+  title: "Department Management System",
+  description: "Department Management System",
 };
 
 export default function RootLayout({
