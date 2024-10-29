6e1a1573a35b0903ea5c001506f853a5ae17c424
Umar <umarabdullah.work@gmail.com>
2024-10-29 11:19:58 +0500

    Search box in Navbar added

--- Front-End/src/components/Navbar.tsx
+++ Front-End/src/components/Navbar.tsx
@@ -1,6 +1,15 @@
+import Image from 'next/image';
 const Navbar = () => {
     return (
-        <div className = ''> Navbar </div>
+        <div className = 'flex items-center justify-between p-4'> 
+        {/* {SEARCH BAR} */}
+         <div className='hidden md:flex'>
+            <Image src="/search.png" alt="" width={14} height={14}/>
+            <input type="text" placeholder='Search...' />
+         </div>
+         {/* {ICONS AND USER} */}
+         <div className=''></div>
+         </div>
     )
 }
 export default Navbar;
