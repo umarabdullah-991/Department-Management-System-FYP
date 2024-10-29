e61f089a783badb98c1a4dee3662c2ed3cd80a2e
Syed <amanxsyed@gmail.com>
2024-10-29 12:08:28 +0500

    Completed the Navbar Section

--- Front-End/src/components/Navbar.tsx
+++ Front-End/src/components/Navbar.tsx
@@ -3,18 +3,18 @@ const Navbar = () => {
     return (
         <div className = 'flex items-center justify-between p-4'> 
         {/* {SEARCH BAR} */}
-         <div className='hidden md:flex'>
+         <div className='hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2'>
             <Image src="/search.png" alt="" width={14} height={14}/>
-            <input type="text" placeholder='Search...' />
+            <input type="text" placeholder='Search...' className='w-[200px] p-2 bg-transparent outline-none '/>
          </div>
          {/* {ICONS AND USER} */}
-         <div className='flex item-center gap-6'>
+         <div className='flex item-center gap-6 justify-end w-full'>
             <div className='bg-white rounded full w-7 h-7 flex items-center justify-center cursor-pointer'>
                 <Image src="/message.png" alt='' width={20} height={20}/>
             </div>
-            <div className='bg-white rounded full w-7 h-7 flex items-center justify-center cursor-pointer'>
+            <div className='bg-white rounded full w-7 h-7 flex items-center justify-center cursor-pointer relative'>
                 <Image src="/announcement.png" alt='' width={20} height={20}/>
-            <div className='absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white relative rounded-full text-xs'>2</div>
+            <div className='absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs'>2</div>
             </div>
             <div className='flex flex-col'>
                 <span className='text-xs leading-3 font-medium'>John Doe</span>
