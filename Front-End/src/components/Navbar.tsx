3dfc1f5587927d2efd1564072e1f36bea2def044
Umar <umarabdullah.work@gmail.com>
2024-10-29 11:39:15 +0500

    Updated navBar, Icons and User avtar and name added

--- Front-End/src/components/Navbar.tsx
+++ Front-End/src/components/Navbar.tsx
@@ -8,7 +8,19 @@ const Navbar = () => {
             <input type="text" placeholder='Search...' />
          </div>
          {/* {ICONS AND USER} */}
-         <div className=''></div>
+         <div className='flex item-center gap-6'>
+            <div className='bg-white rounded full w-7 h-7 flex items-center justify-center cursor-pointer'>
+                <Image src="/message.png" alt='' width={20} height={20}/>
+            </div>
+            <div className='bg-white rounded full w-7 h-7 flex items-center justify-center cursor-pointer'>
+                <Image src="/announcement.png" alt='' width={20} height={20}/>
+            <div className='absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white relative rounded-full text-xs'>2</div>
+            </div>
+            <div className='flex flex-col'>
+                <span className='text-xs leading-3 font-medium'>John Doe</span>
+                <span className='text-[10px] text-gray-500 text-right'>Admin</span>
+            </div>
+         </div>
          </div>
     )
 }
