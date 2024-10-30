c5416a23b1cb63afce016ce258fbcc3657fba1f7
Syed <amanxsyed@gmail.com>
2024-10-30 17:21:56 +0500

    Recharts install from NPM, count charts class is created, Radial Chart code is added in newly count chart file and integrated this countchart.tsx in admin page

--- Front-End/src/app/(dashboard)/admin/page.tsx
+++ Front-End/src/app/(dashboard)/admin/page.tsx
@@ -1,9 +1,10 @@
 import UserCard from "@/components/UserCard"
+import CountChart from "@/components/CountChart"
 const AdminPage = () => {
     return (
         <div className = 'p-4 flex gap-4 flex-col md:flex-row'>
             {/* {LEFT} */}
-            <div  className="w-full lg:w-2/3">
+            <div  className="w-full lg:w-2/3 flex flex-col gap-8">
             {/* {UserCard} */}
                 <div className="flex gap-4 justify-between flex-wrap">
                 <UserCard type="student"/>
@@ -12,6 +13,17 @@ const AdminPage = () => {
                 <UserCard type="staff"/>
                 </div>
             </div>
+            {/* {MIDDLE CHARTS} */}
+            <div className="flex gap-4 flex-col lg:flex-row">
+                {/* COUNT CHART */}
+                <div className="w-full lg:w-1/3 h-[450px]">
+                <CountChart/>
+                </div>
+                {/* ATTENDANCE CHART */}
+                <div className="w-full lg:w-2/3 h-[450px]"></div>
+            </div>
+            {/* {BOTTOM CHARTS} */}
+            <div className=""></div>
             {/* {RIGHT} */}
             <div className="w-full lg:w-1/3">r</div>
         </div>
--- Front-End/src/components/UserCard.tsx
+++ Front-End/src/components/UserCard.tsx
@@ -2,12 +2,12 @@ import Image from "next/image"
 const UserCard = ({type}: {type:string}) => {
     return(
     <div className='rounded-2xl odd:bg-customPurple even:bg-customYellow p-4 flex-1'>
-        <div className="">
-            <span>2024/25</span>
+        <div className="flex justify-between items-center">
+            <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">2024/25</span>
             <Image src="/more.png" alt="" width={20} height={20}/>
         </div>
-        <h1>1,234</h1>
-        <h2>{type}</h2>
+        <h1 className="text-2xl font-semibold my-4">1,234</h1>
+        <h2 className="capitalize text-sm font-medium text-gray-500">{type}</h2>
     </div>
     );
 };
