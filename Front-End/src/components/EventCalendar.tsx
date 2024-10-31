dd3bd760de0ee98082d203ddee5fae76f55fbda5
Umar <umarabdullah.work@gmail.com>
2024-10-31 14:23:49 +0500

    Event stylling added

--- Front-End/src/components/EventCalendar.tsx
+++ Front-End/src/components/EventCalendar.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Image from "next/image";
 import { title } from "process";
 import { useState } from "react";
 import Calendar from "react-calendar";
@@ -33,17 +34,22 @@ const events = [
 const EventCalendar = () => {
     const [value, onChange] = useState<Value>(new Date());
     return(
-        <div className="bg-white p-4 rounded-md"><Calendar onChange={onChange} value={value} />
+        <div className="bg-white p-4 rounded-md">
+            <Calendar onChange={onChange} value={value} />
+            <div className="flex items-center justify-between">
+                <h1 className="text-xl font-semibold my-4">Events</h1>
+                <Image src="/moreDark.png" width={20} height={20} alt="" />
+            </div>
         <div className="flex flex-col gap-4">
             {events.map(event => (
-                <div className="" key={event.id}>
+                <div className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-customBlueLight even:border-t-customPurple" key={event.id}>
                     <div className="flex items-center justify-between">
-                        <h1>{event.title}</h1>
-                        <span>{event.time}</span>
-                        <div>
-                            <p>{event.description}</p>
+                        <h1 className="font-semibold text-gray-600">{event.title}</h1>
+                        <span className="text-gray-300 text-xs ">{event.time}</span>
                         </div>
-                    </div>
+                        <div>
+                            <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
+                        </div>
                 </div>
             ))}
         </div>
