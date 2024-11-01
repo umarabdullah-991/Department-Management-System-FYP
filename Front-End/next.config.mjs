937d8433bdf8f4b1c54b4d6d214a78ca4ae9a220
Syed <amanxsyed@gmail.com>
2024-11-02 02:44:54 +0500

    Responsive Reusable List table, 1 error in next.config.mjs dont know why this happen

--- Front-End/next.config.mjs
+++ Front-End/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    images: {
+      remotePatterns: [{ hostname: "images.pexels.com" }],
+    },
+};
 export default nextConfig;
