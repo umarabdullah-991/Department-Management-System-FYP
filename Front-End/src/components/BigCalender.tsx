54afae20b99261a1c283249e9b3829474e1ed33c
Umar <umarabdullah.work@gmail.com>
2024-11-01 23:12:48 +0500

    Customize calendar toolbar and event styling for improved UI appearance.

--- Front-End/src/components/BigCalender.tsx
+++ Front-End/src/components/BigCalender.tsx
@@ -22,8 +22,8 @@ const BigCalendar = () => {
       view={view}
       style={{ height: "98%" }}
       onView={handleOnChangeView}
-      min={new Date(2021,1,0,8,0,0)}
-      max={new Date(2021,1,0,17,0,0)}
+      min={new Date(2025, 1, 0, 8, 0, 0)}
+      max={new Date(2025, 1, 0, 17, 0, 0)}
 
     />
     );
