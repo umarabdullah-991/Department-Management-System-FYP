d8e49bbb288d50203c2c4a12e489bf77a15754dd
Syed <amanxsyed@gmail.com>
2024-11-02 01:18:00 +0500

    List Table Design with Pagination

---
+++ Front-End/src/app/(dashboard)/list/teachers/page.tsx
@@ -0,0 +1,34 @@
+import Pagination from "@/components/Pagination";
+import TableSearch from "@/components/TableSearch";
+import Image from "next/image";
+
+const TeacherPageList = () => {
+    return(
+        <div className="bg-white rounded-md flex-1 m-4 mt-0 ">
+            {/* TOP */}
+            <div className="flex items-center justify-between">
+                <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+                <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto ">
+                    <TableSearch />
+                    <div className="flex items-center gap-4 self-end">
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-customYellow">
+                            <Image src="/filter.png" alt="" width={14} height={14} />
+                        </button>
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-customYellow">
+                            <Image src="/sort.png" alt="" width={14} height={14} />
+                        </button>
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-customYellow">
+                            <Image src="/plus.png" alt="" width={14} height={14} />
+                        </button>
+                    </div>
+                </div>
+                
+            </div>
+            {/* LIST */}
+            
+            {/* PAGINATION */}
+            <Pagination />
+        </div>
+    );
+}
+export default TeacherPageList;
