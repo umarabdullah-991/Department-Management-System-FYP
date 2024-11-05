7d943c4dcc30dcfbc43ac78ef0af27441dbef02b
Umar <umarabdullah.work@gmail.com>
2024-11-05 20:18:03 +0500

    Updated subjects,parents,classes and student lists

--- Front-End/src/app/(dashboard)/list/lessons/page.tsx
+++ Front-End/src/app/(dashboard)/list/lessons/page.tsx
@@ -1,54 +1,47 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { classesData, role, subjectsData} from "@/lib/data";
+import { lessonsData, role, subjectsData} from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
-type Class = {
+type Lesson = {
     id: number;
-    name: string;
-    capacity: number;
-    grade: number;
-    supervisor: string;
+    subject: string;
+    class: string;
+    teacher: string;
   };
 
 const columns = [
   {
-    header: "Class Name",
+    header: "Subject Name",
     accessor: "name",
   },
   {
-    header: "Capacity",
-    accessor: "capacity",
-    className: "hidden md:table-cell",
+    header: "Class",
+    accessor: "class",
     
   },
   {
-    header: "Grade",
-    accessor: "grade",
+    header: "Teacher",
+    accessor: "teacher",
     className: "hidden md:table-cell",
   },
-  {
-    header: "Supervisor",
-    accessor: "supervisor",
-    className: "hidden md:table-cell",
-  }, 
   {
     header: "Actions",
     accessor: "action",
   },
 ];
 
-const ClassListPage = () => {
-  const renderRow = (item: Class) => (
+const LessonListPage = () => {
+  const renderRow = (item: Lesson) => (
     <tr key = {item.id} 
     className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-customPurpleLight">
       <td className="flex items-center gap-4 p-4">
       {item.name}</td>
-      <td className="hidden md:table-cell">{item.capacity}</td>
-      <td className="hidden md:table-cell">{item.grade}</td>
-      <td className="hidden md:table-cell">{item.supervisor}</td>
+      <td className="hidden md:table-cell">{item.subject}</td>
+      <td>{item.class}</td>
+      <td className="hidden md:table-cell">{item.teacher}</td>
       <td>
         <div className="flex items-center gap-2">
           <td>
@@ -75,7 +68,7 @@ const ClassListPage = () => {
     <div className="bg-white rounded-md flex-1 m-4 mt-0 ">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold"> All Classes </h1>
+        <h1 className="hidden md:block text-lg font-semibold"> All Lessons </h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto ">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -94,11 +87,11 @@ const ClassListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={classesData} />
+      <Table columns={columns} renderRow={renderRow} data={lessonsData} />
 
       {/* PAGINATION */}
       <Pagination />
     </div>
   );
 };
-export default ClassListPage;
+export default LessonListPage;
--- Front-End/src/app/(dashboard)/list/subjects/page.tsx
+++ Front-End/src/app/(dashboard)/list/subjects/page.tsx
@@ -40,7 +40,7 @@ const SubjectListPage = () => {
             <div className="flex items-center gap-2">
               <Link href={`/list/teachers/${item.id}`}>
                 <button className="w-7 h-7 flex items-center justify-center rounded-full bg-customBlueLight">
-                  <Image src="/view.png" alt="" width={16} height={16} />
+                  <Image src="/edit.png" alt="" width={16} height={16} />
                 </button>
               </Link>
               {role === "admin" && (
