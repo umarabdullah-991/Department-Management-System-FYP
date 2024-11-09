a226e85022b0387b50d71441081195671b32bffa
Syed <amanxsyed@gmail.com>
2024-11-09 11:09:56 +0500

    User-Info Card is Completed

--- Front-End/src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ Front-End/src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -9,9 +9,38 @@ const SingleTeacherPage = () => {
           {/* USER INFO CARD */}
           <div className="bg-customBlueLight py-6 px-4 rounded-md flex-1 flex gap-4">
             <div className="w-1/3">
-            <Image src="https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200" alt="" width={144} height={144} className="w-36 h-36 object-cover rounded-full" />
+              <Image
+                src="https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                alt=""
+                width={144}
+                height={144}
+                className="w-36 h-36 object-cover rounded-full"
+              />
             </div>
-            <div className="w-2/3"></div>
+            <div className="w-2/3 flex flex-col justify-between gap-4">
+              <h1 className="text-xl font-semibold">Leonard Snyder</h1>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/blood.png" alt="" width={14} height={14} />
+                  <span>A+</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/date.png" alt="" width={14} height={14} />
+                  <span>january 2025</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/mail.png" alt="" width={14} height={14} />
+                  <span>amanxsyed@gmail.com</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/phone.png" alt="" width={14} height={14} />
+                  <span>+9234567</span>
+                </div>
+              </div>
+            </div>
           </div>
           {/* SMALL CARDS */}
           <div className=""></div>
