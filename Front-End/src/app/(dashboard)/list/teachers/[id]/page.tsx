3e44a56b71189e66d0b94e80250658197dff6d68
Syed <amanxsyed@gmail.com>
2024-11-09 11:51:17 +0500

    Small card are added in the userinfo top, big calendar component is added in bottom div and on the right side of teachers page shortcuts and announcements are added

--- Front-End/src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ Front-End/src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,4 +1,7 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalender";
 import Image from "next/image";
+import Link from "next/link";
 const SingleTeacherPage = () => {
   return (
     <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
@@ -43,13 +46,85 @@ const SingleTeacherPage = () => {
             </div>
           </div>
           {/* SMALL CARDS */}
-          <div className=""></div>
+          <div className="flex-1 flex gap-4 justify-between flex-wrap">
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleAttendance.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">6</h1>
+                <span className="text-sm text-gray-400">Attendance</span>
+              </div>
+            </div>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleBranch.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">2</h1>
+                <span className="text-sm text-gray-400">Branches</span>
+              </div>
+            </div>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleLesson.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">6</h1>
+                <span className="text-sm text-gray-400">Lessons</span>
+              </div>
+            </div>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleClass.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">6</h1>
+                <span className="text-sm text-gray-400">Classes</span>
+              </div>
+            </div>
+          </div>
         </div>
         {/* BOTTOM */}
-        <div className="">Schedule</div>
+        <div className="mt-4 bg-white rounded-md p-4 h-[800%]">
+          <h1>Teacher&apos;s Schedule</h1>
+          <BigCalendar />
+        </div>
       </div>
       {/* RIGHT */}
-      <div className="w-full xl:w-1/3">r</div>
+      <div className="w-full xl:w-1/3 flex flex-col gap-4">
+        <div className="bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Shortcuts</h1>
+          <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+            <Link className="p-3 rounded-md bg-customBlueLight" href="">Teacher&apos;s Classes</Link>
+            <Link className="p-3 rounded-md bg-customPurpleLight" href="">Teacher&apos;s Students</Link>
+            <Link className="p-3 rounded-md bg-customYellowLight" href="">Teacher&apos;s Lessons</Link>
+            <Link className="p-3 rounded-md bg-pink-50" href="">Teacher&apos;s Exams</Link>
+            <Link className="p-3 rounded-md bg-customBlueLight" href="">Teacher&apos;s Assignments</Link>
+          </div>
+        </div>
+        <Announcements />
+      </div>
     </div>
   );
 };
