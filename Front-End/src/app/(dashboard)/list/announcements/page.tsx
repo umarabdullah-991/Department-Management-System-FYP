02a1141185b6d51d6a5a77589016786b713dde07
Umar <umarabdullah.work@gmail.com>
2025-01-08 01:12:22 +0500

    refactor: remove outdated README for Lama Dev and update main README for DIT Management Dashboard

--- Front-End/src/app/(dashboard)/list/announcements/page.tsx
+++ Front-End/src/app/(dashboard)/list/announcements/page.tsx
@@ -16,7 +16,7 @@ const AnnouncementListPage = async ({
   searchParams: { [key: string]: string | undefined };
 }) => {
   
-  const { userId, sessionClaims } = auth();
+  const { userId, sessionClaims } =  auth();
   const role = (sessionClaims?.metadata as { role?: string })?.role;
   const currentUserId = userId;
   
