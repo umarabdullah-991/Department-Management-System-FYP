30469d6d26e0bf2e721f65cefc782aa183959098
Umar <umarabdullah.work@gmail.com>
2025-01-10 00:56:27 +0500

    fix: update StudentForm to use new validation schema and improve form handling; add image upload and additional fields

--- Front-End/src/components/FormModal.tsx
+++ Front-End/src/components/FormModal.tsx
@@ -87,10 +87,10 @@ const forms: {
   ),
   student: (setOpen, type, data, relatedData) => (
     <StudentForm
-      type={type}
-      data={data}
-      setOpen ={setOpen}
-      relatedData={relatedData}
+    type={type}
+    data={data}
+    setOpen={setOpen}
+    relatedData={relatedData}
     />
   ),
   exam: (setOpen, type, data, relatedData) => (
@@ -114,10 +114,10 @@ const FormModal = ({
   const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
   const bgColor =
     type === "create"
-      ? "bg-lamaYellow"
+      ? "bg-customYellow"
       : type === "update"
-      ? "bg-lamaSky"
-      : "bg-lamaPurple";
+      ? "bg- customBlueLight"
+      : "bg-customPurple";
 
   const [open, setOpen] = useState(false);
 
