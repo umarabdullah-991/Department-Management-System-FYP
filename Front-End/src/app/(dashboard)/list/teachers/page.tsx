4bcacce36755bad767f75a7aeab92027377a316b
Syed <amanxsyed@gmail.com>
2025-01-14 23:53:25 +0500

    update student and teacher links for correct routing and implement filtering by classId in teacher list

--- Front-End/src/app/(dashboard)/list/teachers/page.tsx
+++ Front-End/src/app/(dashboard)/list/teachers/page.tsx
@@ -5,7 +5,7 @@ import TableSearch from "@/components/TableSearch";
 import { role, teachersData } from "@/lib/data";
 import prisma from "@/lib/prisma";
 import { ITEM_PER_PAGE } from "@/lib/settings";
-import { Class, Subject, Teacher } from "@prisma/client";
+import { Class, Prisma, Subject, Teacher } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -104,8 +104,28 @@ const TeacherListPage = async ({
 }) => {
   const {page, ...queryParams} = searchParams;
   const p = page ? parseInt(page) : 1;
+  // URL PARAMS CONDITION
+  const query: Prisma.TeacherWhereInput = {};
+
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "classId":
+            query.lessons = {
+              some: {
+                classId: parseInt(value),
+              },
+            };
+        }
+      }
+    }
+  }
+
+
   const [data, count] =  await prisma.$transaction([
   prisma.teacher.findMany({
+    where: query,
     include: {
       subjects: true,
       classes: true
@@ -113,7 +133,7 @@ const TeacherListPage = async ({
     take: ITEM_PER_PAGE,
     skip: ITEM_PER_PAGE * (p - 1),
   }),
-  prisma.teacher.count()
+  prisma.teacher.count({where: query}),
 ]);
   // console.log(data);
   return (
