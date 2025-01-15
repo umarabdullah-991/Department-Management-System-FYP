109fbddea6183d265495d971a112b6da3fb75fb5
Syed <amanxsyed@gmail.com>
2025-01-15 23:48:00 +0500

    event list page to use Prisma for data fetching, implement search functionality, and improve pagination logic; update rendering to display event details

--- Front-End/src/app/(dashboard)/list/events/page.tsx
+++ Front-End/src/app/(dashboard)/list/events/page.tsx
@@ -1,18 +1,14 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { eventsData,role,} from "@/lib/data";
+import { eventsData, role } from "@/lib/data";
+import prisma from "@/lib/prisma";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { Class, Event, Prisma } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
-type Event = {
-    id: number;
-    title: string;
-    class: string;
-    date: string;
-    startTime: string;
-    endTime: string;
-  };
+type EventList = Event & { class: Class };
 
 const columns = [
   {
@@ -20,62 +16,103 @@ const columns = [
     accessor: "title",
   },
   {
-      header: "Class",
-      accessor: "class",
-      
-    },
+    header: "Class",
+    accessor: "class",
+  },
   {
-      header: "Date",
-      accessor: "date",
+    header: "Date",
+    accessor: "date",
     className: "hidden md:table-cell",
   },
   {
     header: "Start Time",
     accessor: "startTime",
-  className: "hidden md:table-cell",
-},
-{
+    className: "hidden md:table-cell",
+  },
+  {
     header: "End Time",
     accessor: "endTime",
-  className: "hidden md:table-cell",
-},
+    className: "hidden md:table-cell",
+  },
 
   {
     header: "Actions",
     accessor: "action",
   },
 ];
+const renderRow = (item: EventList) => (
+  <tr
+    key={item.id}
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-customPurpleLight"
+  >
+    <td className="flex items-center gap-4 p-4">{item.title}</td>
+    <td>{item.class.name}</td>
+    <td className="hidden md:table-cell">{new Intl.DateTimeFormat("en-US").format(item.startTime)}</td>
+    <td className="hidden md:table-cell">{item.startTime.toLocaleTimeString("en-US",{
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false
+    })}</td>
+    <td className="hidden md:table-cell">{item.endTime.toLocaleTimeString("en-US",{
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false
+    })}</td>
+    <td>
+      <div className="flex items-center gap-2">
+        <td>
+          <div className="flex items-center gap-2">
+            <Link href={`/list/teachers/${item.id}`}>
+              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-customBlueLight">
+                <Image src="/edit.png" alt="" width={16} height={16} />
+              </button>
+            </Link>
+            {role === "admin" && (
+              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-customPurple">
+                <Image src="/delete.png" alt="" width={16} height={16} />
+              </button>
+            )}
+          </div>
+        </td>
+      </div>
+    </td>
+  </tr>
+);
+const EventListPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
+  const { page, ...queryParams } = searchParams;
+  const p = page ? parseInt(page) : 1;
+  // URL PARAMS CONDITION
+  const query: Prisma.EventWhereInput = {};
 
-const EventListPage = () => {
-  const renderRow = (item: Event) => (
-    <tr key = {item.id} 
-    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-customPurpleLight">
-      <td className="flex items-center gap-4 p-4">{item.title}</td>
-      <td>{item.class}</td>
-      <td className="hidden md:table-cell">{item.date}</td>
-      <td className="hidden md:table-cell">{item.startTime}</td>
-      <td className="hidden md:table-cell">{item.endTime}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          <td>
-            <div className="flex items-center gap-2">
-              <Link href={`/list/teachers/${item.id}`}>
-                <button className="w-7 h-7 flex items-center justify-center rounded-full bg-customBlueLight">
-                  <Image src="/edit.png" alt="" width={16} height={16} />
-                </button>
-              </Link>
-              {role === "admin" && (
-                <button className="w-7 h-7 flex items-center justify-center rounded-full bg-customPurple">
-                  <Image src="/delete.png" alt="" width={16} height={16} />
-                </button>
-                
-              )}
-            </div>
-          </td>
-        </div>
-      </td>
-    </tr>
-  );
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "search":
+            query.title = { contains: value, mode: "insensitive" };
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  const [data, count] = await prisma.$transaction([
+    prisma.event.findMany({
+      where: query,
+      include: {
+        class: true,
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1),
+    }),
+    prisma.event.count({ where: query }),
+  ]);
 
   return (
     <div className="bg-white rounded-md flex-1 m-4 mt-0 ">
@@ -91,19 +128,19 @@ const EventListPage = () => {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-customYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-           {role === "admin" && (
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-customYellow">
-              <Image src="/plus.png" alt="" width={14} height={14} />
-            </button>
-        )}
+            {role === "admin" && (
+              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-customYellow">
+                <Image src="/plus.png" alt="" width={14} height={14} />
+              </button>
+            )}
           </div>
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={eventsData} />
+      <Table columns={columns} renderRow={renderRow} data={data} />
 
       {/* PAGINATION */}
-      <Pagination />
+      <Pagination page={p} count={count} />
     </div>
   );
 };
