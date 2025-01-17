1aedc98382b6d91ba664624cc2ecb3969598b759
Syed <amanxsyed@gmail.com>
2025-01-17 19:14:32 +0500

    AdminPage to use EventCalendarContainer; update Announcements to fetch data based on user role; create EventCalendarContainer and EventList components for event management

--- Front-End/src/app/(dashboard)/admin/page.tsx
+++ Front-End/src/app/(dashboard)/admin/page.tsx
@@ -1,10 +1,14 @@
 import UserCard from "@/components/UserCard"
 import CountChartContainer from "@/components/CountChartContainer"
 import FinanceChart from "@/components/FinanceChart"
-import EventCalendar from "@/components/EventCalendar"
+import EventCalendarContainer from "@/components/EventCalendarContainer"
 import Announcements from "@/components/Announcements"
 import AttendanceChartContainer from "@/components/AttendanceChartContainer"
-const AdminPage = () => {
+const AdminPage = ({
+  searchParams,
+}: {
+  searchParams: { [keys: string]: string | undefined };
+}) => {
     return (
         <div className = 'p-4 flex gap-4 flex-col md:flex-row'>
              {/* LEFT */}
@@ -34,7 +38,7 @@ const AdminPage = () => {
       </div>
             {/* {RIGHT} */}
             <div className="w-full lg:w-1/3 flex flex-col gap-8">
-                <EventCalendar />
+            <EventCalendarContainer searchParams={searchParams}/>
                 <Announcements/>  
             </div>
         </div>
